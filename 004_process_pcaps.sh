0aea3b5a23043b009f1078b6e4f1a92bb5800724
Markus Kont <markuskont@gmail.com>
2015-11-29 13:21:25 +0200

    Working on Snort implementation.

--- 004_process_pcaps.sh
+++ 004_process_pcaps.sh
@@ -12,7 +12,7 @@ MONITORING_BOX2='moloch'
 SENDER_BOX='host'
 LISTENER_BOX='cnc'
 
-SNORT_LOG_DIR="$LOG_DIR/snort"
+SNORT_ET_LOG_DIR="$LOG_DIR/snort_et"
 BRO_LOG_DIR="$LOG_DIR/bro"
 SURICATA_LOG_DIR="$LOG_DIR/suricata"
 
@@ -20,11 +20,39 @@ EXTENTION='.pcap'
 
 MOLOCH_BASE_DIR="/data/moloch"
 
+# Common functions
+function die() { 
+    echo "$@" 1>&2
+    exit 1
+}
+
+function SSH () {
+    BOX=$1
+    CMD=$2
+
+    vagrant ssh $BOX -c "$CMD"
+
+}
+
+echo "Please enter oinkcode: "
+read OINKCODE
+
 TAP="
 sudo cp /vagrant/elk/logstash/10-read.conf /etc/logstash/conf.d/
 sudo curl -XDELETE localhost:9200/*
 sudo service logstash stop
 sudo service logstash start
+
+if [[ $OINKCODE =~ [a-f0-9]+ ]]; then
+    echo \"Downloading Sourcefire registered rules\"
+    sudo wget -O rules.tgz https://www.snort.org/rules/snortrules-snapshot-2976.tar.gz?oinkcode=$OINKCODE
+    mkdir -p /vagrant/snort/sourcefire
+    rm -r /vagrant/snort/sourcefire/*
+    tar -xzf rules.tgz -C /vagrant/snort/sourcefire
+    cp /vagrant/snort/snort-sourcefire.conf /vagrant/snort/sourcefire/snort.conf
+else
+    echo \"Oinkcode does not match HEX format. Not doing anything.\"
+fi
 
 nohup sudo suricata -c /etc/suricata/suricata.yaml --unix-socket & sleep 10
 
@@ -51,10 +79,24 @@ for pcap in \`find $PCAP_DIR -type f -name *$EXTENTION\`; do
     mkdir -p \$iteration_log_dir
     sudo suricatasc -c \"pcap-file \$pcap \$iteration_log_dir\"
 
+    #echo \"Processing \$pcap with Snort (Emerging threats ruleset)\"
+    #iteration_log_dir=$SNORT_ET_LOG_DIR
+    #if [ -d \$iteration_log_dir ]; then
+    #    rm -r \$iteration_log_dir
+    #fi
+    #mkdir -p \$iteration_log_dir
+
+    #echo \"Processing \$pcap with Snort (Sourcefire registered ruleset)\"
+    #iteration_log_dir=$SNORT_ET_LOG_DIR
+    #if [ -d \$iteration_log_dir ]; then
+    #    rm -r \$iteration_log_dir
+    #fi
+    #mkdir -p \$iteration_log_dir
 done
 
 sudo pkill -9 Suricata
 "
+
 MOLOCH="
 
 sudo pkill -9 moloch
@@ -76,23 +118,8 @@ nohup sudo ./run_viewer.sh & sleep 1
 
 "
 
-# Common functions
-
-function die() { 
-    echo "$@" 1>&2
-    exit 1
-}
-
-function SSH () {
-    BOX=$1
-    CMD=$2
-
-    vagrant ssh $BOX -c "$CMD"
-
-}
-
 SSH $MONITORING_BOX "$TAP"
-SSH $MONITORING_BOX2 "$MOLOCH"
+#SSH $MONITORING_BOX2 "$MOLOCH"
 
 # MAIN
 # suricatasc
